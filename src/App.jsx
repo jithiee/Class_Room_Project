1798778a386f8af5ad9345749efede89bc23f44a
jihtin <129176693+jihtin@users.noreply.github.com>
2024-05-31 16:20:48 +0530

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<h1>hi hello , jithin update........</h1>
-<h1>hi hello , jithin update........</h1>
-<h1>hi hello , jithin update........</h1>
+  <Button/>
+
     </>
   )
 }
