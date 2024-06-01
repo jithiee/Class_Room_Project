6e776eb07612e76134f481fe2b930763fadf9560
jihtin <129176693+jihtin@users.noreply.github.com>
2024-06-01 17:39:30 +0530

    register

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button'
+import Register from './components/Register'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-  <Button/>
-
+  {/* <Button/> */}
+  
+  <Register/>
     </>
   )
 }
