91e56da1fa7a463aeccf75d73ebff0e09296e64a
jihtin <129176693+jihtin@users.noreply.github.com>
2024-06-01 18:23:01 +0530

    update login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Button from './components/Button'
+
+
 import Login from './components/Login'
-import './App.css'
-import Button from './components/Button'
+import Register from './components/Register'
+
+
 
 
 function App() {
@@ -12,9 +12,9 @@ function App() {
 
   return (
  
+    <>
     <Login/>
   
-    <>}
   
   <Register/>
 
